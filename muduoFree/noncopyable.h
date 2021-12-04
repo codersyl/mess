753857b297e44729c2eedd82fa5c24ca210320f0
codersyl <public.syl@outlook.com>
2021-12-04 23:11:59 +0800

    format this file, from 2 space indent to 4 space indent

--- muduoFree/noncopyable.h
+++ muduoFree/noncopyable.h
@@ -2,15 +2,14 @@
 #ifndef _NONCOPYABLE_H_
 #define _NONCOPYABLE_H_
 
-
 class noncopyable
 {
 protected:
-  noncopyable() {}
-  ~noncopyable() {}
+	noncopyable() {}
+	~noncopyable() {}
 private:
-  noncopyable(const noncopyable&);  // 禁用拷贝构造函数
-  const noncopyable& operator=(const noncopyable&); // 禁用赋值构造函数
+	noncopyable(const noncopyable&);  // 禁用拷贝构造函数
+	const noncopyable& operator=(const noncopyable&); // 禁用赋值构造函数
 };
 
 #endif // _NONCOPYABLE_H_

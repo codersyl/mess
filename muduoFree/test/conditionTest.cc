9d654bff557edc2e7c9f90b43b79486b17d2a2f4
codersyl <public.syl@outlook.com>
2021-12-06 20:08:43 +0800

    fix the problem that 2 threads not join before main() end

--- muduoFree/test/conditionTest.cc
+++ muduoFree/test/conditionTest.cc
@@ -12,32 +12,34 @@ Condition buffEvent(mtx);
 
 void consumer()
 {
-	// 消耗 1500个吧
+	// 消耗 1500个
 	std::cout<<"consumer start ---"<<std::endl;
-	// MutexLockGuard a(mtx);
-	// for(int i = 0; i < 1500; i++) {
-	// 	while(v.empty())
-	// 		buffEvent.wait();
-	// 	assert(!v.empty());
-	// 	std::cout<<"consume NO."<<v.size()<<" : "<<v.back()<<std::endl;
-	// 	v.pop_back();
-	// }
+	MutexLockGuard a(mtx);
+	for(int i = 0; i < 1500; i++) {
+		while(v.empty())
+			buffEvent.wait();
+		assert(!v.empty());
+		std::cout<<"consume NO."<<v.size()<<" : "<<v.back()<<std::endl;
+		v.pop_back();
+	}
 }
 
 void producer()
 {
 	// 生产 2000 个
 	std::cout<<"PRODUCER start ---"<<std::endl;
-	// MutexLockGuard b(mtx);
-	// for(int i = 0; i < 2000; i++) {
-	// 	v.push_back(i);
-	// 	std::cout<<"PRODUCE NO."<<v.size()<<" : "<<v.back()<<std::endl;
-	// 	buffEvent.notify();
-	// }
+	MutexLockGuard b(mtx);
+	for(int i = 0; i < 2000; i++) {
+		v.push_back(i);
+		std::cout<<"PRODUCE NO."<<v.size()<<" : "<<v.back()<<std::endl;
+		buffEvent.notify();
+	}
 }
 
 int main() {
 	std::thread first(consumer);
 	std::thread second(producer);
+	first.join();
+	second.join();
 	return 0;
 }

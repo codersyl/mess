ad608b3ee29186d663d62cb8ad49d1e8168294bd
codersyl <public.syl@outlook.com>
2021-12-08 22:41:14 +0800

    4 new files need to check

---
+++ muduoFree/needTest/CurrentThread.cc
@@ -0,0 +1,17 @@
+// need test if it's ok
+#include "CurrentThread.h"
+
+namespace CurrentThread {
+__thread int t_cachedTid = 0;
+__thread char t_tidString[32];
+__thread int t_tidStringLength = 6;
+__thread const char* t_threadName = "default";
+}
+
+void CurrentThread::cacheTid() {
+  if (t_cachedTid == 0) {
+    t_cachedTid = gettid();
+    t_tidStringLength =
+      snprintf(t_tidString, sizeof t_tidString, "%5d ", t_cachedTid);
+  }
+}

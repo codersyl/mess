a82911141bf834e733a938ccb07e06cf3ed33ea3
codersyl <public.syl@outlook.com>
2021-12-12 18:18:27 +0800

    add bash usage

---
+++ linux/bash_usage.sh
@@ -0,0 +1,16 @@
+# 本文记录bash编写入门
+
+# 参考菜鸟教程
+# https://www.runoob.com/linux/linux-shell.html
+# 施工中
+
+# 每个bash第一行都是：
+#!/bin/bash
+
+# 运行方法
+# 方法一：写完后，在命令行：
+chmod +x ./test.sh  #使脚本具有执行权限
+./test.sh  #执行脚本
+# 方法二：
+/bin/sh test.sh
+

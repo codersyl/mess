db123ed095d54f0f7a2dde3911c2e3f7001c6cd8
codersyl <public.syl@outlook.com>
2021-12-12 20:44:39 +0800

    修改了cmake指定的g++编译器版本（不是通过CMakeLists.txt修改，而是通过本地配置修改），加了个清楚runInBuildTest.sh的脚本（即清楚当前目录下所有文件），修改了test中的开头说明

--- muduoFree/test/src/test_EventLoop.cc
+++ muduoFree/test/src/test_EventLoop.cc
@@ -11,7 +11,7 @@ void printTestPropose() {
 本测试会先打印main函数的 pid 与 tid\n\
 然后实例化一个EventLoop\n\
 然后用函数threadFunc()初始化一个Thread\n\
-然后Thread start() loop() join()\n\
+然后Thread_start() EventLoop_loop() Thread_join()\n\
 最终结束函数\n\
 ---------------------------\n");
 }

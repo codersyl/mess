2bf12f3d917b2e105dc0a8b819054c27db4d32af
codersyl <public.syl@outlook.com>
2021-12-21 22:45:20 +0800

    尝试定位事发现场

--- muduoLearning/test/testCode/testP289.cc
+++ muduoLearning/test/testCode/testP289.cc
@@ -14,20 +14,29 @@ void timeout() {
 }
 
 int main() {
+	printf("start!\n");
 	EventLoop loop;
+	printf("EventLoop in main() constructed!\n");
 	g_loop = &loop;
+	printf("g_loop = &loop;!\n");
 	int timerfd = ::timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC);
+	printf("timerfd got!\n");
 	Channel channel(&loop, timerfd);
+	printf("channel constructed!\n");
 	channel.setReadCallback(timeout);
+	printf("ReadCallback set!\n");
 	channel.enableReading();
-
+	printf("enableReading()!\n");
 	struct itimerspec howlong;
+	printf("howlong constructed!\n");
 	bzero(&howlong, sizeof(howlong));
+	printf("howlong bzero!\n");
 	howlong.it_value.tv_sec = 5;
+	printf("howlong set 5 seconds!\n");
 	::timerfd_settime(timerfd, 0, &howlong, NULL);
-
+	printf("::timerfd_settime()\n");
 	loop.loop();
+	printf("loop end!\n");
 	::close(timerfd);
-
 	return 0;
 }

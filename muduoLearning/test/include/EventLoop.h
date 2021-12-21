e26c41f7c13976e3a4d672d8c9223c71ebb184c5
codersyl <public.syl@outlook.com>
2021-12-21 17:01:01 +0800

    修了几个错误，准备重试

--- muduoLearning/test/include/EventLoop.h
+++ muduoLearning/test/include/EventLoop.h
@@ -4,11 +4,12 @@
 #include "Thread.h"
 #include "CurrentThread.h"
 #include "noncopyable.h"
+#include "Channel.h"
+#include "EPoller.h"
 #include <memory>
 #include <assert.h>
+#include <vector>
 
-class Channel;
-class EPoller;
 
 class EventLoop : noncopyable
 {
